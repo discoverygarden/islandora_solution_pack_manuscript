9919fb88bfa660af7c8040a069e48455fac6492b
Adam Vessey <adam@discoverygarden.ca>
2014-07-11 19:55:30 +0000

    Allow selection of containers, disallow selection of components.

--- js/jstree.js
+++ js/jstree.js
@@ -5,11 +5,27 @@
 
 (function ($) {
   Drupal.behaviors.islandora_manuscript_jstree = {
-    attach: function(context, settings) {
-      for (var id in settings.islandora_manuscript.jstree.info) {
-        $('#' + id).jstree(settings.islandora_manuscript.jstree.info[id]).bind('select_node.jstree', function (evt, data) {
-          
-        });
+    attach: function (context, settings) {
+      if (typeof settings.islandora_manuscript != 'undefined') {
+        for (var id in settings.islandora_manuscript.jstree.info) {
+          $('#' + id).once('islandora-manuscript-jstree', function () {
+            $.extend(true, settings.islandora_manuscript.jstree.info[id], {
+              'conditionalselect': function (node) {
+                return this.get_type(node, true)['select_node'];
+              }
+            });
+            $(this)
+              .jstree(settings.islandora_manuscript.jstree.info[id])
+              .bind('select_node.jstree', function (evt, data) {
+                // Stash the selected node, so we can get associated info when
+                //processing PHP-side.
+                data.instance.element.closest('form').find('input[name="selected_node"]').val(JSON.stringify(data.node));
+              })
+              .closest('form')
+              .find('input[name="selected_node"]')
+              .val('');
+          });
+        }
       }
     }
   };
