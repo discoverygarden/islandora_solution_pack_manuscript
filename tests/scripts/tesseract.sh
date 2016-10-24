bcebf790d3814b839a2c71bf84908d9d4a77c082
willtp87 <willtp87@hotmail.com>
2016-10-24 11:15:38 -0300

    Removing another ref to google code

--- tests/scripts/tesseract.sh
+++ tests/scripts/tesseract.sh
@@ -16,9 +16,7 @@ cd tesseract-ocr
 make && checkinstall --pkgname=tesseract-ocr --pkgversion="3.02.02" --backup=no --deldoc=yes --fstrans=no --default && ldconfig
 mkdir ~/tesseract/langs
 cd ~/tesseract/langs
-wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
-echo "Extracting language files"
-for i in *.tar.gz; do echo -e "\nworking on $i\n"; tar xvzf $i ; done
-cp tesseract-ocr/tessdata/* /usr/local/share/tessdata/
+wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.traineddata
+cp eng.traineddata /usr/local/share/tessdata/
 echo -e "\ntesseract output:"
 tesseract --version && tesseract --list-langs && cd ~ && rm -rf ~/tesseract
