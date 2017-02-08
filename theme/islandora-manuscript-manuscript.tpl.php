f0f62824de9bec72619d92cfa2e65eca2fdb2c68
vagrant <vagrant@ryanb-vagrant-uconn.48597.dgicloud.com>
2017-02-08 15:22:49 -0400

    Adding metadata display for Manuscript Solution Pack

--- theme/islandora-manuscript-manuscript.tpl.php
+++ theme/islandora-manuscript-manuscript.tpl.php
@@ -9,4 +9,19 @@
     <?php print $viewer; ?>
   </div>
 <?php endif; ?>
-<!-- @todo Add table of metadata values -->
+<?php if ($display_metadata): ?>
+    <div class="islandora-collection-metadata">
+      <?php print $description; ?>
+      <?php if ($parent_collections): ?>
+          <div>
+              <h2><?php print t('In collections'); ?></h2>
+              <ul>
+                <?php foreach ($parent_collections as $collection): ?>
+                    <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+                <?php endforeach; ?>
+              </ul>
+          </div>
+      <?php endif; ?>
+      <?php print $metadata; ?>
+    </div>
+<?php endif; ?>
