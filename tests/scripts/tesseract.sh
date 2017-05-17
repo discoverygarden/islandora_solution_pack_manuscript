e0157835b5bddd8872c11c7a1fc9a0728469aa7f
willtp87 <willtp87@hotmail.com>
2017-05-17 14:11:02 -0300

    tesseract updates

--- tests/scripts/tesseract.sh
+++ tests/scripts/tesseract.sh
@@ -2,18 +2,18 @@ apt-get update
 apt-get install build-essential checkinstall 
 mkdir ~/tesseract
 cd ~/tesseract
-wget http://www.leptonica.org/source/leptonica-1.69.tar.gz
-tar xf leptonica-1.69.tar.gz && rm -rf leptonica-1.69.tar.gz
-cd leptonica-1.69
+wget http://www.leptonica.org/source/leptonica-1.73.tar.gz
+tar xf leptonica-1.73.tar.gz && rm -rf leptonica-1.73.tar.gz
+cd leptonica-1.73
 ./configure
-make && checkinstall --pkgname=libleptonica --pkgversion="1.69" --backup=no --deldoc=yes --fstrans=no --default
+make && checkinstall --pkgname=libleptonica --pkgversion="1.73" --backup=no --deldoc=yes --fstrans=no --default
 cd ~/tesseract
-wget https://github.com/tesseract-ocr/tesseract/archive/3.02.02.tar.gz
-tar xf 3.02.02.tar.gz && rm -rf 3.02.02.tar.gz
-cd tesseract-3.02.02
+wget https://github.com/tesseract-ocr/tesseract/archive/3.04.01.tar.gz
+tar xf 3.04.01.tar.gz && rm -rf 3.04.01.tar.gz
+cd tesseract-3.04.02
 ./autogen.sh
 ./configure
-make && checkinstall --pkgname=tesseract-ocr --pkgversion="3.02.02" --backup=no --deldoc=yes --fstrans=no --default && ldconfig
+make && checkinstall --pkgname=tesseract-ocr --pkgversion="3.04.01" --backup=no --deldoc=yes --fstrans=no --default && ldconfig
 mkdir ~/tesseract/langs
 cd ~/tesseract/langs
 wget https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/eng.traineddata
